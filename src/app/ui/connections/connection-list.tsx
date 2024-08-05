805918f0efc6764f5ec7624e3ce77ac24afc6a09
Gustavo <gustavo.motta23@gmail.com>
2024-08-05 18:45:08 -0300

    feat: Improved connection list style

--- src/app/ui/connections/connection-list.tsx
+++ src/app/ui/connections/connection-list.tsx
@@ -1,27 +1,36 @@
 import { getConnections } from "@/lib/actions";
 import Link from "next/link";
+import {
+  ArrowTopRightOnSquareIcon,
+  TrashIcon,
+  NoSymbolIcon,
+  CheckIcon,
+  CheckBadgeIcon,
+} from "@heroicons/react/24/solid";
 
 export async function ConnectionList() {
   const connections = await getConnections();
   return (
-    <div className="bg-white shadow overflow-hidden sm:rounded-md">
+    <div className="text-left bg-white shadow overflow-hidden sm:rounded-md w-[calc(100vw_-_10rem)]">
       <ul>
         <li className="border-t border-gray-200">
-          <div className="grid grid-cols-5 gap-4 px-4 py-4 sm:px-6 font-bold">
+          <div className="grid grid-cols-[20%_30%_10%_15%_10%_15%] gap-4 px-4 py-4 sm:px-6 font-bold">
             <div className="text-md leading-5 text-blue-500 truncate">Name</div>
             <div className="text-md leading-5 text-blue-500 truncate">Host</div>
             <div className="text-md leading-5 text-blue-500 truncate">Port</div>
             <div className="text-md leading-5 text-blue-500 truncate">
               Database
             </div>
+            <div className="text-md leading-5 text-blue-500 truncate">SSL</div>
+
             <div className="text-md leading-5 text-blue-500 truncate">
-              Action
+              Actions
             </div>
           </div>
         </li>
         {connections.map((db, index) => (
           <li key={index} className="border-t border-gray-200">
-            <div className="grid grid-cols-5 gap-4 px-4 py-4 sm:px-6">
+            <div className="grid grid-cols-[20%_30%_10%_15%_10%_15%] gap-4 px-4 py-4 sm:px-6">
               <div className="text-sm leading-5 font-medium text-blue-500 truncate">
                 {db.name ?? `${db.host}:${db.port}/${db.database}`}
               </div>
@@ -35,11 +44,19 @@ export async function ConnectionList() {
                 {db.database}
               </div>
               <div className="text-sm leading-5 font-medium text-blue-500 truncate">
-                <Link href={`/connections/${db.id}`}>
-                  <div className="text-blue-500 hover:text-indigo-900">
-                    View
-                  </div>
-                </Link>
+                {db.ssl ? (
+                  <CheckBadgeIcon className="h-6 w-6" />
+                ) : (
+                  <NoSymbolIcon className="h-6 w-6" />
+                )}
+              </div>
+              <div className="text-sm leading-5 font-medium text-blue-500 truncate">
+                <div className="flex justify-start gap-x-8">
+                  <Link href={`/connections/${db.id}`}>
+                    <ArrowTopRightOnSquareIcon className="h-6 w-6" />
+                  </Link>
+                  <TrashIcon className="h-6 w-6" />
+                </div>
               </div>
             </div>
           </li>
