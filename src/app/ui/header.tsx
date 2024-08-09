f855b01c1ad47e0ec1ba8626a425e83377b5539b
Gustavo <gustavo.motta23@gmail.com>
2024-08-09 18:23:10 -0300

    feat: Icons

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="bg-gray-800 py-4">
-      <div className="flex space-x-10 container px-10">
+      <div className="flex items-center gap-8 container px-10">
+        <Link href="/">
+          <Image src="/postgre.png" alt="logo" width={50} height={50} />
+        </Link>
         <Link href="/">
           <h1 className="text-white text-2xl font-bold">DumpMyPG</h1>
         </Link>
